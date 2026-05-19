db3eb6c2ec75b263baebaf82a9d2b20e6652104f
Mick Dekkers <m.dekkers@labdigital.nl>
2026-05-19 09:26:19 +0200

    chore: make knip pass

--- knip.ts
+++ knip.ts
@@ -1,6 +1,9 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
+	// Some source files are directly imported by other projects, so we consider
+	// them entry points to disable warnings about unused exports.
+	entry: ["src/**/*.ts"],
 	ignoreDependencies: ["tsx"],
 	tags: ["-lintignore"],
 };
